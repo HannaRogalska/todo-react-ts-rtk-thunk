533dcfabe3ba456cb5bcc7a89b779a3cf345ac36
Hanna Rogalska <hanrogalska@gmail.com>
2025-08-24 16:13:39 +0200

    fixed  fetch function

--- src/features/todo/todoApi.ts
+++ src/features/todo/todoApi.ts
@@ -4,10 +4,6 @@ import type { Todo } from "./todoTypes";
 const API_URL = "https://jsonplaceholder.typicode.com/todos/?_limit=10";
 
 export const fetchTodoApi = async () => {
-  try {
     const res = await axios.get(API_URL);
     return res.data as Todo[];
-  } catch (error) {
-    console.log(error);
-  }
 };
